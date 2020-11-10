4b58d9ba18f2e96d5e8e7dcb9ab2f4860241f2d2
hu.yongan <hu.yongan@TAVE-PC20.OSDBSESTO.local>
2020-11-10 13:03:00 +0100

    Add endless loop

---
+++ src/provagit/ProvaGit.java
@@ -0,0 +1,14 @@
+package provagit;
+
+public class ProvaGit {
+
+    public static void main(String[] args) {
+        int i=0;
+        
+        while (true) {
+            System.out.println("Ehi, ciao! "+i);
+            i++;
+        }
+    }
+    
+}
