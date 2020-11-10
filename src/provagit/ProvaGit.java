5ea21459ad0c8f5115dc6e4b4e8c28b0c49d9feb
hu.yongan <hu.yongan@TAVE-PC20.OSDBSESTO.local>
2020-11-10 13:10:27 +0100

    Initialize i as double

--- src/provagit/ProvaGit.java
+++ src/provagit/ProvaGit.java
@@ -3,7 +3,7 @@ package provagit;
 public class ProvaGit {
 
     public static void main(String[] args) {
-        int i=0;
+        double i=0;
         
         while (true) {
             System.out.println("Ehi, ciao! "+i);
